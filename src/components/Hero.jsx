8da3c5243162f65a9fba6e673650e9fbeb77d110
Mhmdmnsouri <Bitshawcoder@gmail.com>
2024-09-30 01:02:26 +0330

    modify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,14 +30,14 @@ const Hero = () => {
           </p>
           <img
             src={profile}
-            className="sm:hidden w-60 rounded-lg mt-14 ml-6"
+            className="sm:hidden w-60 rounded-lg mt-14 ml-2"
             alt="description"
           />
         </div>
       </div>
       {/* <ComputersCanvas /> */}
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-22 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
